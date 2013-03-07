33e9009f396d25080bb5987a311fce9daff51762
JasperH <jasper.hawinkel@student.kuleuven.be>
2013-03-07 01:24:02 +0100

    Constructorheaders toegevoegd aan 'Ship'

--- Asteroïds/src/Game/Ship.java
+++ Asteroïds/src/Game/Ship.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package Game;
 
 import asteroids.IShip;
@@ -10,5 +7,26 @@ import asteroids.IShip;
  * @version 0.1
  */
 public class Ship implements IShip {
-
+	
+	/**
+	 * 
+	 */
+	public Ship(){
+	
+		
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param xVelocity
+	 * @param yVelocity
+	 * @param radius
+	 * @param angle
+	 */
+	public Ship(double x, double y, double xVelocity, double yVelocity, double radius, double angle ) {
+		
+	}
+	
+	
 }
